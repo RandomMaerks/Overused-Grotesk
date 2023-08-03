06a095ddc6c27c791feb28ef8ad4328a326a1570
RandomMaerks <109415614+RandomMaerks@users.noreply.github.com>
2023-08-03 23:41:27 +0700

    Delete source directory

--- source/build_instances.sh
+++
@@ -1,8 +0,0 @@
-py -m fontmake -m OG.designspace -i OverusedGrotesk-Light
-py -m fontmake -m OG.designspace -i OverusedGrotesk-Book
-py -m fontmake -m OG.designspace -i OverusedGrotesk-Roman
-py -m fontmake -m OG.designspace -i OverusedGrotesk-Medium
-py -m fontmake -m OG.designspace -i OverusedGrotesk-SemiBold
-py -m fontmake -m OG.designspace -i OverusedGrotesk-Bold
-py -m fontmake -m OG.designspace -i OverusedGrotesk-ExtraBold
-py -m fontmake -m OG.designspace -i OverusedGrotesk-Black
